be3efad15cc4d4b27e50a4e8e19802ebbfb88f96
user <user@dcim.com>
2021-08-31 09:46:06 +0200

    vendor:publish --provider="Spatie"

--- config/app.php
+++ config/app.php
@@ -139,6 +139,8 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Spatie\Permission\PermissionServiceProvider::class,
+
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
